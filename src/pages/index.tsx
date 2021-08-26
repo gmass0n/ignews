a48591d9a550a4d8282c86dd7949e53f9fedaca5
Gabriel Masson <g.masson010@gmail.com>
2021-08-26 12:20:31 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,38 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import { Header } from "../components/Header";
+
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from "../styles/home.module.scss";
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Ínicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>hello home</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+
+          <h1>
+            News about
+            <br />
+            the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all the publications
+            <br />
+            <span>for $9.99/month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 };
