fd17d0081a1e9828e9e62f61655ca890593788ff
Gabriel Masson <g.masson010@gmail.com>
2021-08-26 11:24:46 -0300

    feat: add initial structure

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import { AppProps } from "next/app";
+
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
+};
+
+export default MyApp;
